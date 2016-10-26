9808317b7caa791dc9194a80d8186ad5336735d1
Jonathan Thom and Tracie Weitzman <tracie@>
2016-10-26 13:13:01 -0700

    Fix the consonant function to handle multiple consonants at beginning of word.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // business
-var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 var consonants = ["b", "B", "c", "C", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"];
 
 function hasNumbers(originalSentence) {
@@ -9,19 +9,17 @@ function hasNumbers(originalSentence) {
 function pigLatin(originalSentence) {
   var sentenceArray = originalSentence.split("");
 
-  // vowels.forEach(function(vowel) {
-  //   if (vowel === sentenceArray[0]) {
-  //     // sentenceArray.push("ay");
-  //   }
-  // });
+// this only works if the letters are in the order they are in the array.
+  for (i = 0; i < sentenceArray.length; i++) {
+    consonants.forEach(function(consonant) {
+      if (consonant === sentenceArray[0]) {
+        var consonantArray = sentenceArray.shift();
+        sentenceArray.push(consonantArray);
+        // sentenceArray.push("ay");
+      }
+    });
+ }
 
-  consonants.forEach(function(consonant) {
-    if (consonant === sentenceArray[0]) {
-      var consonantArray = sentenceArray.shift();
-      sentenceArray.push(consonantArray);
-      // sentenceArray.push("ay");
-    }
-  });
 
   sentenceArray.push("ay");
   finalSentence = sentenceArray.join("");
