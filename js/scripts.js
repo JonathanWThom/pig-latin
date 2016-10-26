ea283197ab55fc1c2f1e410071038f0b839097fd
Jonathan Thom and Tracie Weitzman <tracie@>
2016-10-26 15:24:40 -0700

    change variables to be more descriptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,49 @@
 // business
-//var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 var consonants = ["b", "B", "c", "C", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"];
 
 function hasNumbers(originalSentence) {
   return /\d/.test(originalSentence);
 }
 
-
 function pigLatin(originalSentence) {
-  var superFinalSentence = [];
+  var finalSentence = [];
   var wordsArray = originalSentence.split(" ");
   console.log(wordsArray);
 
   wordsArray.forEach(function(word) {
 
-    var sentenceArray = word.split("");
+    var lettersArray = word.split("");
 
-    if (((sentenceArray[0]) === "Q" || (sentenceArray[0]) === "q") && ((sentenceArray[1]) === "U" || (sentenceArray[1]) === "u")) {
-      var qArray = sentenceArray.shift();
-      sentenceArray.push(qArray);
-      var finalQArray = sentenceArray.shift();
-      sentenceArray.push(finalQArray);
+    if (((lettersArray[0]) === "Q" || (lettersArray[0]) === "q") && ((lettersArray[1]) === "U" || (lettersArray[1]) === "u")) {
+      var qArray = lettersArray.shift();
+      lettersArray.push(qArray);
+      var finalQArray = lettersArray.shift();
+      lettersArray.push(finalQArray);
     };
 
-    if (((sentenceArray[0]) === "S" || (sentenceArray[0]) === "s") && ((sentenceArray[1]) === "Q" || (sentenceArray[1]) === "q")) {
-      var SqArray = sentenceArray.shift();
-      sentenceArray.push(SqArray);
-      var secondSqArray = sentenceArray.shift();
-      sentenceArray.push(secondSqArray);
-      var finalSqArray = sentenceArray.shift();
-      sentenceArray.push(finalSqArray);
+    if (((lettersArray[0]) === "S" || (lettersArray[0]) === "s") && ((lettersArray[1]) === "Q" || (lettersArray[1]) === "q")) {
+      var SqArray = lettersArray.shift();
+      lettersArray.push(SqArray);
+      var secondSqArray = lettersArray.shift();
+      lettersArray.push(secondSqArray);
+      var finalSqArray = lettersArray.shift();
+      lettersArray.push(finalSqArray);
     };
 
-      for (i = 0; i < sentenceArray.length; i++) {
+      for (i = 0; i < lettersArray.length; i++) {
         consonants.forEach(function(consonant) {
-          if (consonant === sentenceArray[0]) {
-            var consonantArray = sentenceArray.shift();
-            sentenceArray.push(consonantArray);
+          if (consonant === lettersArray[0]) {
+            var consonantArray = lettersArray.shift();
+            lettersArray.push(consonantArray);
           }
         });
      }
-
-      sentenceArray.push("ay");
-      var finalSentence = sentenceArray.join("");
-      console.log(finalSentence);
-      //return finalSentence;
-
-      superFinalSentence.push(finalSentence);
-
+      lettersArray.push("ay");
+      var finalWord = lettersArray.join("");
+      finalSentence.push(finalWord);
     });
-  var superDuperFinalSentence = superFinalSentence.join(" ");
-  console.log(superDuperFinalSentence);
-  return superDuperFinalSentence;
-
+  var output = finalSentence.join(" ");
+  return output;
 }
 
 
